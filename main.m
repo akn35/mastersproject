b6ba4cfa39fbb1ac8693865c865df69ba12bc634
Ankita <akn35@cam.ac.uk>
2021-03-18 22:30:25 +0000

    Notch filter in main script

--- main.m
+++ main.m
@@ -1,3 +1,13 @@
 clear
+
+%Importing Intan Data
 addpath("functions\")
 intanDataImport()
+
+%notch(50, 30000)
+wo =50/(30000/2);
+bw = wo/35;
+[b,a] = iirnotch(wo,bw);
+amplifier_data_notch = filtfilt(b,a,amplifier_data);
+figure(1)
+plot(t(1:end),amplifier_data_notch(1,1:end))
