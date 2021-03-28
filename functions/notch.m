b738b2853589d229909204ad2b76b998a20e9e00
Ankita <akn35@cam.ac.uk>
2021-03-28 22:29:57 +0100

    nf defined variables

--- functions/notch.m
+++ functions/notch.m
@@ -1,7 +1,8 @@
-function notch(time, data, f_notch, fs)
+function notch(t, data, f_notch, fs)
+%notch (t, data, notch filter frequency, sampling frequency)
 wo =f_notch/(fs/2);
 bw = wo/35;
 [b,a] = iirnotch(wo,bw);
 amplifier_data_notch = filtfilt(b,a, data);
 figure(1)
-plot(time(1:end),amplifier_data_notch(1,1:end))
+plot(t(1:end),amplifier_data_notch(1,1:end))
--- main.m
+++ main.m
@@ -1,7 +1,11 @@
 clear
+addpath("functions\")
 
 %Importing Intan Data
-addpath("functions\")
 intanDataImport()
 
-notch(t, amplifier_data ,50, 30000)
+%notch filter
+%parameters 
+f_notch = 50;
+fs = 30000;
+notch(t, amplifier_data, f_notch, fs)
