9ca98a8d5abdba7b63208e7eee36bba24aac4a11
Ankita <akn35@cam.ac.uk>
2021-05-03 16:59:05 +0100

    added channel parameter to notch filter

--- functions/notch.m
+++ functions/notch.m
@@ -1,8 +1,8 @@
-function amplifier_data_notch = notch(t, data, f_notch, fs)
+function amplifier_data_notch = notch(t, data, f_notch, fs ,channel)
 %notch (t, data, notch filter frequency, sampling frequency)
 wo =f_notch/(fs/2);
 bw = wo/35;
 [b,a] = iirnotch(wo,bw);
 amplifier_data_notch = filtfilt(b,a, data);
 figure(1)
-plot(t(1:end),amplifier_data_notch(1,1:end))
+plot(t(1:end),amplifier_data_notch(channel,1:end))
