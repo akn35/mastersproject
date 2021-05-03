f98aed9fca51fe15034e7d4374a546e9a81188c6
Ankita <akn35@cam.ac.uk>
2021-05-03 17:04:46 +0100

    fixed bandpass

--- functions/bandpass_highfreq.m
+++ functions/bandpass_highfreq.m
@@ -1,5 +1,4 @@
-function bandpass_250_to_4k_Hz = bandpass_highfreq (f0, f3, f4, fs, t, data)
-bpFilt2= designfilt('bandpassiir', 'FilterOrder', f0, 'HalfPowerFrequency1', f3, 'HalfPowerFrequency2', f4, 'SampleRate', fs);
-bandpass_250_to_4k_Hz = filter(bpFilt2, data);
+function bandpass_250_to_4k_Hz = bandpass_highfreq(f3, f4, fs, t, data)
+bandpass_250_to_4k_Hz = bandpass(data,[f3 f4], fs); 
 figure (3)
 plot(t(1:end),bandpass_250_to_4k_Hz(1,1:end))
--- functions/bandpass_lowfreq.m
+++ functions/bandpass_lowfreq.m
@@ -1,5 +1,4 @@
-function bandpass_1_to_300_Hz = bandpass_lowfreq (f0, f1, f2, fs, t, data)
-bpFilt1 = designfilt('bandpassiir', 'FilterOrder', f0, 'HalfPowerFrequency1', f1, 'HalfPowerFrequency2', f2, 'SampleRate', fs);
-bandpass_1_to_300_Hz = filter(bpFilt1, data);
+function bandpass_1_to_300_Hz = bandpass_lowfreq(f1, f2, fs, t, data)
+bandpass_1_to_300_Hz = bandpass(data, [f1 f2], fs);
 figure (2)
 plot(t(1:end),bandpass_1_to_300_Hz(1,1:end))
