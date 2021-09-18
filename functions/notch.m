b4ff6f3d6fb3b014ba8bd6901ed92fff30d82101
anks <77733843+akn35@users.noreply.github.com>
2021-09-18 14:44:02 +0100

    updated for figure display purposes

--- functions/notch.m
+++ functions/notch.m
@@ -4,5 +4,6 @@ wo =f_notch/(fs/2);
 bw = wo/35;
 [b,a] = iirnotch(wo,bw);
 amplifier_data_notch = filtfilt(b,a, data);
-figure(1)
+figure;hold on;
+tiledlayout (1,1)
 plot(t(1:end),amplifier_data_notch(channel,1:end))
