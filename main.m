c0f4f5a0f6531741c5bcbeadba3b333100ca09e2
anks <77733843+akn35@users.noreply.github.com>
2021-09-20 23:33:08 +0100

    updates function inputs and commented out unnec code

--- main.m
+++ main.m
@@ -12,11 +12,10 @@ fs = 30000; %sampling freq
 %notch filter only
 
     %notch filter single
-    channel = 1; 
-    amplifier_data_notch = notch(t, amplifier_data, f_notch, fs, 1);
-
+    amplifier_data_notch = notch(amplifier_data, f_notch, fs);
+    
     %notch filer all
-    amplifier_data_notch_all = notch_all(t, amplifier_data, f_notch, fs, 1, 0);
+%   amplifier_data_notch_all = notch_all(amplifier_data, f_notch, fs);
 
 %stasios code
 %parameters
@@ -31,27 +30,27 @@ f4 = 4000;
 %bandpassfilter single - low + high
     
     %low freq  1 to 300 Hz
-    filter_1_to_300_Hz = filter_lowfreq(f1, f2, fs, t, amplifier_data, 1);
+    filter_1_to_300_Hz = filter_lowfreq(f1, f2, fs, amplifier_data);
     
     %low freq  1 to 300 Hz + high freq 250 to 4k Hz
-    bandpass_single = filter_highfreq(f3, f4, fs, t, filter_1_to_300_Hz, 1);
+%   bandpass_single = filter_highfreq(f3, f4, fs, filter_1_to_300_Hz);
     
 %individual filters and notch combined (single channel and maybe add all
 %for each)
 
     %low freq + notch
-    filter_lowfreq_notch = filter_lowfreq(f1, f2, fs, t, amplifier_data_notch, 1);
+    filter_lowfreq_notch = filter_lowfreq(f1, f2, fs, amplifier_data_notch);
 
     %high freq + notch
-    filter_highfreq_notch = filter_highfreq(f3, f4, fs, t, amplifier_data_notch, 1);
+%   filter_highfreq_notch = filter_highfreq(f3, f4, fs, amplifier_data_notch);
 
 %bandpass filter and notch combined (single channel and all channels)
     
     %bandpassfilter single channel - low freq + high freq + notch
-    bandpass_notch_single = filter_highfreq(f3, f4, fs, t, filter_lowfreq_notch, 1);
+%   bandpass_notch_single = filter_highfreq(f3, f4, fs, filter_lowfreq_notch);
 
     %bandpassfilter all channels - low + high freq + notch
-    bandpass_notch_all = filter_highfreq_all(f3, f4, fs, t, filter_lowfreq_notch, 1, 0);
+%   bandpass_notch_all = filter_highfreq_all(f3, f4, fs, filter_lowfreq_notch);
 
 %interface
 interfacedesign()
