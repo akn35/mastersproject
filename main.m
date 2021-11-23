6c42f7ba4bce9a00397a81b1f4da5c5ce6d3a245
Ankita <akn35@cam.ac.uk>
2021-11-23 06:15:23 +0000

    formatting and cleaned up

--- main.m
+++ main.m
@@ -3,55 +3,33 @@ clear
 addpath("functions\")
 addpath("UI\")
 
-%Importing Intan Data
-importIntanData()
+%ImportIntanData
+importIntanData;
+            
+%interface
+interfacedesign()
 
-%parameters 
-f_notch = 50; %notch filter freq
-fs = 30000; %sampling freq
+%Filters
 
 %notch filter only
 
-%notch filter single
-amplifier_data_notch = notch(amplifier_data, f_notch, fs);
+    %notch filter single
+    %amplifier_data_notch = notch(amplifier_data, f_notch, fs);
+    
+%Both bandpass filters only 
 
-%%%%notch filer all
-%amplifier_data_notch_all = notch_all(amplifier_data, f_notch, fs);
+    %low freq  1 to 300 Hz bandpass
+    %filter_1_to_300_Hz = filter_lowfreq(200, 4000, fs, amplifier_data_notch);
 
-%parameters
-%fn = fs/2; %Nyquist freq
-%f0 = 6; %order
- f1 = 1;
- f2 = 100;
- f3 = 50;
- f4 = 4000;
+    %high freq 250 to 4k Hz bandpass
+    %filter_250_to_4k_Hz = filter_highfreq(f3, f4, fs, amplifier_data_notch);
 
-% 2 bandpass filters for single channel buttons
+%Both bandpass filters + notch each individually
 
-%low freq  1 to 300 Hz bandpass
-%filter_1_to_300_Hz = filter_lowfreq(200, 4000, fs, amplifier_data_notch);
+    %low freq bandpass + notch
+    %filter_lowfreq_notch = filter_lowfreq(f1, f2, fs, amplifier_data_notch);
 
-%lhigh freq 250 to 4k Hz bandpass
-%filter_250_to_4k_Hz = filter_highfreq(f3, f4, fs, amplifier_data_notch);
-
-%%%%%%individual filters and notch combined (single channel and maybe add all
-%for each)
-
-%low freq bandpass + notch
-%filter_lowfreq_notch = filter_lowfreq(f1, f2, fs, amplifier_data_notch);
-
-%high freq bandpass + notch
-%filter_highfreq_notch = filter_highfreq(f3, f4, fs, amplifier_data_notch);
-
-%low and high bandpass filters and notch combined (single channel and all channels)
-
-%bandpassfilter single channel - low freq + high freq + notch
-%bandpass_notch_single = filter_highfreq(f3, f4, fs, filter_lowfreq_notch);
-
-%%%%%%bandpassfilter all channels - low + high freq + notch
-%   bandpass_notch_all = filter_highfreq_all(f3, f4, fs, filter_highfreq_notch);
-
-%interface
-interfacedesign()
+    %high freq bandpass + notch
+    %filter_highfreq_notch = filter_highfreq(f3, f4, fs, amplifier_data_notch);
 
 %openExample('matlab/TopLevelMultiwindowAppExample','supportingFile','DialogAppExample');appdesigner('MainAppExample');
