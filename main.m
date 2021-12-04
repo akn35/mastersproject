4096eabcc9c0affb518be5ea12a93f827e547fbf
Ankita <akn35@cam.ac.uk>
2021-12-04 23:29:11 +0000

    main script tidy, removed unused functions and defined filter testing variables

--- main.m
+++ main.m
@@ -10,25 +10,28 @@ interfacedesign()
 
 %Filters
 
+%Params
+
+    %f_notch = 50;
+    %fs = 30000;
+    %f1 = 1;
+    %f2 = 300;
+    %channel = 1;
+       
 %notch filter only
 
-    %notch filter single
     %amplifier_data_notch = filter_notch(amplifier_data, f_notch, fs);
     
-%Both bandpass filters only 
+%bandpass filter only
 
-    %low freq  1 to 300 Hz bandpass
-    %filter_1_to_300_Hz = filter_bandpass(amplifier_data_notch, 200, 4000, fs);
+    %amplifier_data_bandpass = filter_bandpass(amplifier_data, f1, f2, fs);
 
-    %high freq 250 to 4k Hz bandpass
-    %filter_250_to_4k_Hz = filter_bandpass(amplifier_data_notch, f3, f4, fs);
+%notch and bandpass filter
 
-%Both bandpass filters + notch each individually
-
-    %low freq bandpass + notch
-    %filter_bandpass_notch = filter_bandpass(amplifier_data_notch, f1, f2, fs);
-
-    %high freq bandpass + notch
-    %filter_bandpass_notch = filter_bandpass(amplifier_data_notch, f3, f4, fs);
+    %amplifier_data_notch_&_bandpass = filter_bandpass(amplifier_data_notch, f1, f2, fs);
+    
+%plot single channel
+    
+    %plot(t(1:end), amplifier_data_notch(channel, 1:end));
 
 %openExample('matlab/TopLevelMultiwindowAppExample','supportingFile','DialogAppExample');appdesigner('MainAppExample');
